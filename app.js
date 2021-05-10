fbb5d35671e1916e01c747d187fbdfa6dfc8e13e
Harshit Jaiswal (@harshitj183) <76927137+harshitj183@users.noreply.github.com>
2021-05-10 22:33:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('https://raw.githubusercontent.com/harshitj183/Pwa/main/sw.js')
-    .then(reg => console.log('service worker registered'))
-    .catch(err => console.log('service worker not registered', err));
-}
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+const Users = () => {
+ // state
+ const [users, setUsers] = React.useState([]);
+ // effects
+ React.useEffect(() => {
+   fetch("https://jsonplaceholder.typicode.com/users")
+     .then((res) => res.json())
+     .then((users) => {
+       setUsers(users);
+     })
+     .catch((err) => {});
+ }, []);
+ // render
+ return (
+   <div>
+     <h2>Users</h2>
+     <ul>
+       {users.map((user) => (
+         <li key={user.id}>
+           {user.name} ({user.email})
+         </li>
+       ))}
+     </ul>
+   </div>
+ );
+};
+const App = () => (
+ <BrowserRouter>
+   <Route path="/" exact component={Users} />
+ </BrowserRouter>
+);
+export default App;
