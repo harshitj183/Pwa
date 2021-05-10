b9ed956e70c838af5340990eb9e7c795537518d9
Harshit Jaiswal (@harshitj183) <76927137+harshitj183@users.noreply.github.com>
2021-05-10 07:43:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,7 @@
-const staticCacheName = 'site-static-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/assets/js/ui.js',
-  '/assets/css/main.css',
-  'https://1.bp.blogspot.com/-6SGalRDdz1U/YBzJPX22y9I/AAAAAAAAGv8/rBc6KTuJrT4Tj7coYnQEzkl_UnWHnpz4wCLcBGAsYHQ/s512/android-chrome-512x512.png',
-  'https://fonts.googleapis.com/css?family=Lato:300,400,700',
-];
-// install event
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
-});
-// activate event
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-// fetch event
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
-});
+
+```javascript
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('<add-path-here>/pwabuilder-sw.js');
+  });
+```
