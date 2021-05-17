3405d73adba663e4e0a1edc775226dfcb8f41bc1
Harshit Jaiswal (@harshitj183) <76927137+harshitj183@users.noreply.github.com>
2021-05-17 16:20:51 +0530

    Update and rename sw.js to service-worker.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-
-```javascript
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('<add-path-here>/pwabuilder-sw.js');
-  });
-```
